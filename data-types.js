6c735bb6079ad417ae7a7ab742275c824a64d09e
savlana <108239589+savlana@users.noreply.github.com>
2022-07-17 07:14:58 -0500

    Add files via upload

---
+++ data-types.js
@@ -0,0 +1,32 @@
+
+const myName ="Svitlana";
+console.log("name", myName, typeof name);
+
+const age = 27;
+console.log("age", age, typeof age);
+
+const loveCats = true;
+console.log("love cats", loveCats, typeof true);
+
+const teachersFavoriteColor = null;
+console.log("teacher's fav color", teachersFavoriteColor);
+
+const grade = undefined;
+console.log("homework grade", grade);
+
+const student = {
+    myName: "Svitlana",
+    age: 27,
+    loveCats: true,
+};
+console.log(student);
+
+const id = Symbol('id');
+console.log(id);
+
+const phoneNumber = 123456789n;
+console.log("contact", phoneNumber);
+
+
+
+
